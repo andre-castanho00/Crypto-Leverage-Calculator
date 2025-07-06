1a8b2191ccaa8687cf1d75b325d5b76b3a05b005
André Castanho <127101863+andre-castanho00@users.noreply.github.com>
2025-07-06 16:16:48 +0100

    minor changes

--- src/components/imageLoader/imageLoader.jsx
+++ src/components/imageLoader/imageLoader.jsx
@@ -2,11 +2,11 @@ import "./imageLoader.css";
 
 function ImageLoader() {
   return (
-    <div class="fade-images">
-      <img class="fade-img" src="/logos/btc_logo.png" alt="1" />
-      <img class="fade-img" src="/logos/eth_logo.svg" alt="2" />
-      <img class="fade-img" src="/logos/xrp_logo.png" alt="3" />
-      <img class="fade-img" src="/logos/sol_logo.png" alt="4" />
+    <div className="fade-images">
+      <img className="fade-img" src="/logos/btc_logo.png" alt="1" />
+      <img className="fade-img" src="/logos/eth_logo.svg" alt="2" />
+      <img className="fade-img" src="/logos/xrp_logo.png" alt="3" />
+      <img className="fade-img" src="/logos/sol_logo.png" alt="4" />
     </div>
   );
 }
